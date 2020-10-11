ee8653edfe976073137449288cf12f41e253e929
Concetto Pollicina <concetto.pollicina@gmail.com>
2020-10-11 09:43:20 +0200

    debug(welcome): add debug print

--- core/database/repository/user.py
+++ core/database/repository/user.py
@@ -1,6 +1,14 @@
 from core.database.db_connect import Connection
 
 class UserRepository(Connection):
-    def getById(self,args=None):
+    def getById(self, args=None):
         sql = "SELECT * FROM users WHERE user_id = %s"
         return self._selectAll(sql, args)
+
+    def add(self, args=None):
+        # TODO: write add function
+        return ''
+
+    def update(self, username):
+        # TODO: write update function
+        return ''
--- core/handlers/welcome.py
+++ core/handlers/welcome.py
@@ -2,38 +2,76 @@ import re
 from core import decorators
 from languages.getLang import languages
 from core.database.repository.group import GroupRepository
-from core.utilities.message import message
+from core.database.repository.user import UserRepository
+from core.database.repository.superban import SuperbanRepository
+from core.utilities.message import message, reply_message
 from core.utilities import functions
 from core.utilities.functions import delete_message
 from telegram.ext.dispatcher import run_async
+from core.utilities.functions import kick_user, ban_user
 
-def welcome_message(update,context,user):
+def has_arabic_character(string):
+    arabic = re.search("[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+", string)
+    return not not arabic
+
+def save_user(user):
+    # Salva l'utente nel database e controlla che esiste se esiste e ha cambiato nickname sovrascrivi
+    user = UserRepository.getById(user.id)
+    if user:
+        UserRepository.update(username = user.username)
+    else:
+        UserRepository.add(user)
+
+def is_in_blacklist(uid):
+    return not not SuperbanRepository.getByIdFetchOne(uid)
+
+def welcome_user(update, context, member):
+    # Controlla che il welcome esista sul database se non esiste Default Welcome
     chat = update.effective_message.chat_id
-    rows = GroupRepository().getById([chat])
-    for row in rows:
-        if row is not None:
-            parsed_message = row['welcome_text'].replace('{first_name}',
-            update.message.from_user.first_name).replace('{chat_name}',
-            update.message.chat.title).replace('{username}',"@"+user.username)
-            format_message = "{}".format(parsed_message)
-            welcome = update.message.reply_text(format_message,parse_mode='HTML')
-            return True
-        else:
-            return False
-    return welcome
 
-def arabic_filter(update,context,user_first_name):
-    arabic = re.search("[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+",user_first_name)
-    if arabic:
-        return True
-    return arabic
+    groups = GroupRepository().getById(chat)
+    if groups is not None:
+        group = groups[0]
+
+        parsed_message = group['welcome_text'].replace(
+            '{first_name}',
+            update.message.from_user.first_name).replace('{chat_name}',
+            update.message.chat.title).replace('{username}',
+            "@"+member.username
+        )
+        format_message = "{}".format(parsed_message)
+        # welcome = update.message.reply_text(format_message,parse_mode='HTML')
+        reply_message(format_message)
+    else:
+        reply_message('default welcome')
+
+
+def welcome_bot(update, context):
+    # TODO: handler che salva il gruppo suò database e controlla che esiste. se esiste e ha cambiato id lo cambia
+    reply_message(update, context, "Grazie per avermi aggiunta al gruppo :D")
 
 @run_async
 def init(update, context):
     for member in update.message.new_chat_members:
-        ar_filter = arabic_filter(update,context,member.first_name)
-        welcome = welcome_message(update,context,member)
-        if ar_filter == True:
-            functions.kick_user(update,context)
-        if welcome == False:
-            update.message.reply_text("DEFAULT",parse_mode='HTML')
+        print('***Welcome*** ' + str(member))
+
+        if member.is_bot:
+            print('***Welcome*** ' + 'is_bot')
+            welcome_bot(update, context)
+
+        else:
+            print('***Welcome*** ' + 'is_user')
+            # save_user(member)
+
+            if member.username is None:
+                print('***Welcome*** ' + 'no username')
+                kick_user(update, context)
+
+            # TODO: add flag blacklist active
+            elif is_in_blacklist(member.id) or has_arabic_character(member.username):
+                print('***Welcome*** ' + 'blacklist or arabic')
+                ban_user(update, context)
+
+            else:
+                print('***Welcome*** ' + 'welcome')
+                welcome_user(update, context, member)
