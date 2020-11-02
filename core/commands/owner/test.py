b9149188206859c893605e0844ee22bd58871b37
hersel91 <hersel1991@gmail.com>
2020-11-02 21:30:26 +0100

    Test INSERT pt2

--- core/commands/owner/test.py
+++ core/commands/owner/test.py
@@ -2,16 +2,21 @@ from core.database.repository.group import GroupRepository
 from core.database.db_connect import Connection
 from core import decorators
 
+#metodo di INSERT
 def insert_query(query,args):
     connector = Connection()
     print(args)
     sql = connector.cur.executemany(query,args)
     return sql
 
+
 @decorators.owner.init
 def init(update, context):
     chat = str(update.effective_chat.id)
-    test = "banana"
-    data = [(chat,test)]
-    query = "INSERT INTO prova (testo,testo_due) VALUES (%s,%s)"
+    welcome = "Lorem Ipsum Dolor"
+    rules = "Example Rules"
+    community = 1
+    lang = "EN"
+    data = [(chat,welcome,rules,community,lang)]
+    query = "INSERT INTO groups (id_group, welcome_text, rules_text, community, languages) VALUES (%s,%s,%s,%s,%s)"
     insert_query(query,data)
