3b2e074ba3a62b93e039248864e63a4aab1487c3
BluLupo <hersel1991@gmail.com>
2020-12-02 22:37:28 +0100

    update library and fix compatibility

--- core/handlers/handlers_index.py
+++ core/handlers/handlers_index.py
@@ -12,6 +12,6 @@ from telegram.ext import (
 def core_handlers(dsp):
     function = dsp.add_handler
     function(MH(Filters.status_update.new_chat_members, handlers.welcome.init, run_async=True))
-    function(MH(Filters.group, handlers.superban.init, run_async=True))
+    function(MH(Filters.chat_type.groups, handlers.superban.init, run_async=True))
     function(CQH(handlers.welcome.select_language_en, pattern='select_language_en'))
     function(CQH(handlers.welcome.select_language_it, pattern='select_language_it'))
