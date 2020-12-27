2cf1890b7174d5a10adc8da2102eb40d53d2b5b0
BluLupo <hersel1991@gmail.com>
2020-12-27 12:24:21 +0100

    fix settings

--- core/commands/admin/settings.py
+++ core/commands/admin/settings.py
@@ -3,18 +3,20 @@ from core.utilities.menu import build_menu
 from languages.getLang import languages
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from core.database.repository.group import GroupRepository
+from telegram.ext import CallbackQueryHandler
 
-#TODO Devo poter tornare la funzione keyboard_settings al posto di query.edit... per un menu dinamico in tempo reale.
-def keyboard_settings(update,context):
+def keyboard_settings(update,context,editkeyboard = False):
     bot = context.bot
-    chat = update.effective_message.chat_id
+    chat = update.message.chat_id
     group = GroupRepository().getById(chat)
     list_buttons = []
     list_buttons.append(InlineKeyboardButton('Welcome %s' % ('✅' if group['set_welcome'] == 1 else '❌'), callback_data='setWelcome'))
     list_buttons.append(InlineKeyboardButton("Close", callback_data='close'))
     menu = build_menu(list_buttons,2)
-    keyboard_menu = bot.send_message(chat,"Group Settings",reply_markup=InlineKeyboardMarkup(menu),parse_mode='HTML')
-    #keyboard_menu = update.message.reply_text("Group Settings",reply_markup=InlineKeyboardMarkup(menu),parse_mode='HTML')
+    if editkeyboard == False:
+        keyboard_menu = bot.send_message(chat,"Group Settings",reply_markup=InlineKeyboardMarkup(menu),parse_mode='HTML')
+    if editkeyboard == True:
+        keyboard_menu = bot.edit_message_reply_markup(chat,update.message.message_id,reply_markup=InlineKeyboardMarkup(menu))
     return keyboard_menu
 
 @decorators.admin.user_admin
@@ -31,11 +33,10 @@ def update_settings(update,context):
         if row == 1:
             data = [(0,chat)]
             GroupRepository().SetWelcome(data)
-            query.edit_message_text("You have set welcome to off",parse_mode='HTML')
+            return keyboard_settings(query,context,True)
         else:
             data = [(1,chat)]
             GroupRepository().SetWelcome(data)
-            query.edit_message_text("You have set welcome to active",parse_mode='HTML')
-        #return keyboard_settings(query,context)
+            return keyboard_settings(query,context,True)
     if query.data == 'close':
         query.edit_message_text("You have closed the settings menu",parse_mode='HTML')
