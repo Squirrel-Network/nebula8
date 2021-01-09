abcaf9b2e9296bd1c6b0bb92a74a5f9c948f64c1
BluLupo <hersel1991@gmail.com>
2021-01-09 16:20:30 +0100

    more fix

--- core/commands/index.py
+++ core/commands/index.py
@@ -8,13 +8,18 @@ from telegram.ext import (CommandHandler as CMH,CallbackQueryHandler as CQH,Mess
 
 def user_command(dsp):
     function = dsp.add_handler
+    ######################
+    ### CommandHandler ###
+    ######################
     function(CMH('start', public.start.init))
     function(CMH('rules', public.rules.init))
     function(CMH('source', public.source.init))
-    function(MH(Filters.text("@admin"), public.report.init))
 
 def admin_command(dsp):
     function = dsp.add_handler
+    ######################
+    ### CommandHandler ###
+    ######################
     function(CMH('ban', admin.ban.init))
     function(CMH('status', admin.info_group.init))
     function(CMH('lang', admin.set_lang.init))
@@ -24,12 +29,18 @@ def admin_command(dsp):
     function(CMH('say', admin.say.init))
     function(CMH('settings', admin.settings.init))
     function(CMH('delete', admin.delete_message.init))
+    #############################
+    ### CallbackQuery Handler ###
+    #############################
     function(CQH(admin.set_lang.language_en, pattern='language_en'))
     function(CQH(admin.set_lang.language_it, pattern='language_it'))
     function(CQH(admin.settings.update_settings))
 
 def owner_command(dsp):
     function = dsp.add_handler
+    ######################
+    ### CommandHandler ###
+    ######################
     function(CMH('b', owner.broadcast.init, run_async=True))
     function(CMH('s', owner.superban.init, run_async=True))
     function(CMH('server', owner.server_info.init))
--- core/handlers/handlers_index.py
+++ core/handlers/handlers_index.py
@@ -3,9 +3,29 @@
 
 # Copyright SquirrelNetwork
 from core import handlers
+from core.commands import public
 from telegram.ext import (MessageHandler as MH,Filters)
+from core.utilities.functions import chat_object, user_object
+
+def test_handler(update,context):
+    a = print("NO EXE ALLOWED")
+    return a
+
+def terminal_handler(update):
+    user = user_object(update)
+    chat = chat_object(update)
+    text = str(update.effective_message.text)
+    msg = "FIRST_NAME: {}\nTG_ID: {}\nCHAT_ID: {}\nCHAT_TITLE: {}\nMESSAGE: {}".format(user.first_name,user.id,chat.id,chat.title,text)
+    a = print(msg)
+    return a
 
 def core_handlers(dsp):
     function = dsp.add_handler
+    function(MH(Filters.text("@admin"), public.report.init))
+    function(MH(Filters.document.exe, test_handler))
     function(MH(Filters.status_update.new_chat_members, handlers.welcome.init, run_async=True))
-    function(MH(Filters.chat_type.groups, handlers.check_status_user.check_status, run_async=True))
+    function(MH(Filters.chat_type.groups, group_handlers, run_async=True))
+
+def group_handlers(update,context):
+    handlers.check_status_user.check_status(update,context)
+    terminal_handler(update)
