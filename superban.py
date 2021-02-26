58419403f58a9f60e77d6b2de4a3345ca75db393
BluLupo <hersel1991@gmail.com>
2021-02-26 20:41:29 +0100

    repair codacy badge

---
+++ superban.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright SquirrelNetwork
+
+from core.database.repository.superban import SuperbanRepository
+from languages.getLang import languages
+from core.utilities.message import message
+from core.utilities.functions import ban_user
+from core.utilities.functions import delete_message
+
+def init(update, context):
+    user = update.effective_user.id
+    rows = SuperbanRepository().getById([user])
+    if rows:
+        print("superban")
+        msg = "I got super banned <code>{}</code>".format(user)
+        message(update,context,msg)
+        delete_message(update,context)
+        ban_user(update,context)
