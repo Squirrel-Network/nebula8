5bffc561528c6c0f860d2a0a91383917dfd190b4
BluLupo <hersel1991@gmail.com>
2021-03-20 22:53:26 +0100

    update and fix /weather

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,4 +1,5 @@
 from pyowm import OWM
+from pyowm.commons import exceptions
 from config import Config
 from core import decorators
 from core.utilities.message import message
@@ -15,37 +16,30 @@ def sendWeatherMessage(update,context,city,mintemp,maxtemp,humidity,icon):
 def init(update, context):
     text = update.message.text[8:].strip().capitalize()
     if text != "":
-        owm = OWM(Config.OPENWEATHER_API)
-        mgr = owm.weather_manager()
-        observation = mgr.weather_at_place(text)
-        w = observation.weather
-        temp = w.temperature('celsius')
-        mintemp = temp['temp_min']
-        maxtemp = temp['temp_max']
-        humidity = w.humidity
-        status = w.detailed_status
-        print(status)
-        if status == 'clear sky':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} ☀️'.format(status))
-        elif status == 'broken clouds':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} ⛈'.format(status))
-        elif status == 'scattered clouds':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌤'.format(status))
-        elif status == 'few clouds':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌦'.format(status))
-        elif status == 'overcast clouds':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} ☁️'.format(status))
-        elif status == 'light rain':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌧'.format(status))
-        elif status == 'moderate rain':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌧'.format(status))
-        elif status == 'rain':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌧'.format(status))
-        elif status == 'smoke':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌫'.format(status))
-        elif status == 'mist':
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'{} 🌫'.format(status))
-        else:
-            sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,status)
+        try:
+            owm = OWM(Config.OPENWEATHER_API)
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(text)
+            w = observation.weather
+            temp = w.temperature('celsius')
+            mintemp = temp['temp_min']
+            maxtemp = temp['temp_max']
+            humidity = w.humidity
+            status = w.status
+            print(status)
+            if status == 'Clear':
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'☀️')
+            elif status == 'Clouds':
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'☁️')
+            elif status == 'Rain':
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'🌧')
+            elif status == 'Drizzle':
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'🌧')
+            elif status == 'Mist':
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,'🌫')
+            else:
+                sendWeatherMessage(update,context,text,mintemp,maxtemp,humidity,status)
+        except exceptions.NotFoundError:
+            message(update,context, text="The city you searched for does not exist!")
     else:
         message(update,context, text="You need to type a search criteria!\nHow to use the command: <code>/weather text</code>")
