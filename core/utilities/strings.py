88b2aed33d9f7b79357b32a9cba3fc6a3468398e
BluLupo <hersel1991@gmail.com>
2021-06-28 00:08:04 +0200

    minimal fix

--- core/utilities/strings.py
+++ core/utilities/strings.py
@@ -8,7 +8,7 @@ class Strings(object):
     BAN_LOG = "<b>#Log User Banned!</b>\nUser_Id: {id}\n"\
               'Username: <a href="tg://user?id={id}">{username}</a>\n'\
               "Group: {chat}\n"
-    SUPERBAN_LOG = "<b>#Log User Superbanned!</b>\nUser_Id: {}\nReason: {}\nDatetime: <code>{}</code>\nOperator: {} [{}]\nKnowhere Query: https://squirrel-network.online/knowhere/?q={}"
+    SUPERBAN_LOG = "<b>#Log User Superbanned!</b>\nUser_Id: {}\nReason: {}\nDatetime: <code>{}</code>\nOperator: {} [{}]"
     REPORT_MSG = "#Report\nGroup Id: <code>{}</code>\nGroup Title: {}\nMessage: <i>{}</i>\nLink: {}"
     SOURCE = "<b>Hi! my name is: {}\nMy Version is: <code>{} {}</code>\nMy repository is: {}</b>"
     USER_INFO = '<b>INFO USER:</b>\nUserId: <code>{id}</code>\nUsername: <a href="tg://user?id={id}">{username}</a>\nWarn: <code>{warn}</code>\nGroup: {chat}'
