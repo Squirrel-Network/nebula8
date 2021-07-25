47440048293b41a08805d8b11b1df0d319ccb0d4
BluLupo <hersel1991@gmail.com>
2021-07-25 18:49:50 +0200

    fix test.py

--- core/commands/owner/test.py
+++ core/commands/owner/test.py
@@ -3,10 +3,7 @@
 
 # Copyright SquirrelNetwork
 from core import decorators
-from config import Config
 
-OWNERS_K = list(Config.OWNER.keys())
-OWNERS_V = list(Config.OWNER.values())
 @decorators.owner.init
 def init(update,context):
-    print("Gli Owners del bot sono:\n{} [{}]".format(OWNERS_K,OWNERS_V))
+    pass
