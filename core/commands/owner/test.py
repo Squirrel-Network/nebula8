27b19749a83b037bd73f9c50de99ca51e8567ef9
BluLupo <hersel1991@gmail.com>
2021-08-25 15:28:06 +0200

    add experimental function with ASYNCIO

--- core/commands/owner/test.py
+++ core/commands/owner/test.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Copyright SquirrelNetwork
+import asyncio
 from core import decorators
+from core.utilities.message import messageWithAsync
+
+loop = asyncio.get_event_loop()
 
 @decorators.owner.init
 def init(update,context):
-    pass
+      loop.run_until_complete(messageWithAsync(update,context,1,"ASYNC TEST"))
--- core/utilities/message.py
+++ core/utilities/message.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Copyright SquirrelNetwork
+import asyncio
+
 def message(update,context,text = ""):
     bot = context.bot
     chat = update.effective_chat.id
@@ -21,3 +23,11 @@ def PrivateMessage(update,context, text = ""):
     bot = context.bot
     msg = bot.send_message(update.message.from_user.id,text,parse_mode='HTML')
     return msg
+
+async def messageWithAsync(update,context,delay,text = ""):
+    bot = context.bot
+    chat = update.effective_chat.id
+    await asyncio.sleep(delay)
+    msg = bot.send_message(chat,text,parse_mode='HTML')
+    return msg
+    
