b9094ed61a69fecc6de742eec43442edce458053
BluLupo <hersel1991@gmail.com>
2021-10-01 09:57:02 +0200

    add new constant in constants.py

--- core/utilities/constants.py
+++ core/utilities/constants.py
@@ -26,3 +26,25 @@ PERM_TRUE = ChatPermissions(
     can_invite_users=False,
     can_pin_messages=False
     )
+
+PERM_MEDIA_TRUE = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages=True,
+    can_send_polls=True,
+    can_send_other_messages=True,
+    can_add_web_page_previews=True,
+    can_change_info=False,
+    can_invite_users=False,
+    can_pin_messages=False
+    )
+
+PERM_MEDIA_FALSE = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages=False,
+    can_send_polls=True,
+    can_send_other_messages=True,
+    can_add_web_page_previews=True,
+    can_change_info=False,
+    can_invite_users=False,
+    can_pin_messages=False
+    )
