ac69d583f8b7386f47368986262dd737e783de70
BluLupo <hersel1991@gmail.com>
2022-02-07 21:15:01 +0100

    /dashboard pt1

--- core/commands/admin/dashboard.py
+++ core/commands/admin/dashboard.py
@@ -4,6 +4,7 @@
 # Copyright SquirrelNetwork
 
 from core.utilities.functions import member_status_object, chat_status_object
+from core.utilities.message import message
 
 def init(update,context):
     reply = update.message.reply_to_message
@@ -14,9 +15,10 @@ def init(update,context):
         user_status = member_status_object(update,context)
         chat_status = chat_status_object(update, context)
         if user_status.status == 'creator':
-            print(user_status)
-            print(chat_status)
-            print("CREATOR OK")
+            user = user_status.user
+            username = "@"+user.username
+            data = [user.id,user.first_name,username,chat_status.id]
+            message(update,context,"<i>UNDER CONSTRUCTION {}</i>\n\nSeguici su: https://github.com/Squirrel-Network/nebula8".format(data))
         else:
-            print("NO CREATOR")
-        
+            msg = "Non sei il proprietario del gruppo non puoi usare questo comando"
+            message(update,context,msg)
