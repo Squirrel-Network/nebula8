412cc208481a838248e63d05fc665da99eca53da
BluLupo <hersel1991@gmail.com>
2022-02-21 16:52:09 +0100

    minor update

--- core/commands/admin/dashboard.py
+++ core/commands/admin/dashboard.py
@@ -11,9 +11,8 @@ from core.utilities.message import message
 def init(update,context):
     reply = update.message.reply_to_message
     if reply:
-        print("REPLY")
+        message(update,context,"Questo comando al momento non può essere utilizzato in risposta ad un messaggio!")
     else:
-        print("NO REPLY")
         user_status = member_status_object(update,context)
         chat_status = chat_status_object(update, context)
         if user_status.status == 'creator':
@@ -22,9 +21,12 @@ def init(update,context):
             save_date = datetime.datetime.utcnow().isoformat()
             row = DashboardRepository().getById(user.id)
             if row:
-                print("UPDATE")
-                data = [(username, save_date, user.id, chat_status.id)]
-                DashboardRepository().update(data)
+                if row['enable'] == 0:
+                    message(update,context,"Mi dispiace sei stato disabilitato a questa funzionalità, Contatta un amministratore su: https://t.me/nebulabot_support")
+                else: 
+                    data = [(username, save_date, user.id, chat_status.id)]
+                    DashboardRepository().update(data)
+                    message(update,context,"Ho aggiornato i dati sul database!")
             else:
                 data = [(user.id,username,chat_status.id,1,save_date,save_date)]
                 DashboardRepository().add(data)
--- plugins/distrowatch.py
+++ plugins/distrowatch.py
@@ -6,14 +6,19 @@
 import requests
 from core import decorators
 from bs4 import BeautifulSoup
+from core.utilities.menu import build_menu
 from core.utilities.message import message
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 @decorators.delete.init
 def init(update, context):
+    bot = context.bot
     r = requests.get("https://distrowatch.com/random.php")
     parsed_html = BeautifulSoup(r.text, features="html.parser")
     distro_long_name = parsed_html.title.string[17:].lower()
     distro_name = distro_long_name.split()[0]
     distro_url = f'https://distrowatch.com/table.php?distribution={distro_name}'
-    distro_message = "Here is a random linux distribution: {}".format(distro_url)
-    message(update,context,distro_message)
+    distro_message = "Here is a random linux distribution: "
+    button_list = [InlineKeyboardButton("🐧 ▶️ ", url=distro_url)]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
+    bot.send_message(update.message.chat_id,text=distro_message,reply_markup=reply_markup,parse_mode='HTML')
