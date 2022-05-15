36b53f9a0427e474973558885f367aee879970bf
BluLupo <hersel1991@gmail.com>
2022-05-15 15:59:28 +0200

    fix custom_handler

--- core/commands/admin/set_custom_handler.py
+++ core/commands/admin/set_custom_handler.py
@@ -15,10 +15,13 @@ def init(update, context):
     msg = update.message.text[7:]
     reply = update.message.reply_to_message
     if reply:
-        custom_question = str(reply.text).lower()
-        custom_answer = str(msg).lower()
-        data = [(chat.id,custom_question,custom_answer)]
-        GroupRepository().insert_custom_handler(data)
-        message(update,context,"<b>Custom handler setted!</b>\n\nQuestion: <code>{}</code>\nAnswer: <code>{}</code>".format(custom_question,custom_answer))
+        custom_question = str(reply.text)
+        if custom_question == '/test':
+            message(update, context, 'This command is reserved for the bot!')
+        else:
+            custom_answer = str(msg).lower()
+            data = [(chat.id,custom_question,custom_answer)]
+            GroupRepository().insert_custom_handler(data)
+            message(update,context,"<b>Custom handler setted!</b>\n\nQuestion: <code>{}</code>\nAnswer: <code>{}</code>".format(custom_question,custom_answer))
     else:
         message(update,context,"You must reply to a message to use this command!")
--- core/handlers/custom_handler.py
+++ core/handlers/custom_handler.py
@@ -9,22 +9,7 @@ from core.database.repository.group import GroupRepository
 from telegram.utils.helpers import mention_html
 
 def init(update,context):
-    if str(update.effective_message.text).lower().startswith("nebula"):
-        chat = chat_object(update)
-        user = user_object(update)
-        question = str(update.message.text.lower())
-        row = GroupRepository().get_custom_handler([question,chat.id])
-        if row:
-            parsed_message = row['answer'].replace('{first_name}',
-            user.first_name).replace('{chat}',
-            update.message.chat.title).replace('{username}',
-            "@"+user.username).replace('{mention}',mention_html(user.id, user.first_name)).replace('{userid}',str(user.id))
-            text = "{}".format(parsed_message)
-            message(update,context,text)
-        else:
-            return
-
-    if str(update.effective_message.text).lower().startswith("!"):
+    if str(update.effective_message.text).lower().startswith("nebula") or str(update.effective_message.text).lower().startswith("!") or str(update.effective_message.text).lower().startswith("/"):
         chat = chat_object(update)
         user = user_object(update)
         question = str(update.message.text.lower())
