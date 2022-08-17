b785c9df524afbf39926f9cf3a987fe0e8b91c09
BluLupo <hersel1991@gmail.com>
2022-08-18 00:39:25 +0200

    fix /pin

--- core/commands/admin/pin.py
+++ core/commands/admin/pin.py
@@ -13,8 +13,18 @@ def pin(update,context):
     reply = update.message.reply_to_message
     if reply:
         msg = str(reply.text).strip()
-        bot.send_message(update.message.chat_id, text=msg, parse_mode='HTML')
-        bot.pin_chat_message(update.message.chat_id, update.message.message_id+1,disable_notification=True)
+        if reply.photo:
+            #file_id = reply.photo[2].file_id
+            #get_file = bot.get_file(file_id)
+            #url = get_file['file_path']
+            #print(url)
+            #bot.sendPhoto(chat_id=update.effective_chat.id, photo=url, caption="sddsdsdsds", parse_mode='HTML')
+            #message(update, context, "caption", 'HTML', 'photo', None, url)
+            #bot.pin_chat_message(update.message.chat_id, update.message.message_id+1,disable_notification=True)
+            message(update,context,"You can't pin an image")
+        else:
+            message(update,context,msg)
+            bot.pin_chat_message(update.message.chat_id, update.message.message_id+1,disable_notification=True)
     else:
         msg = update.message.text[4:].strip()
         message(update,context,msg)
